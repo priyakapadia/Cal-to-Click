e578f3cf0888f8e68d5c121a59e24aa1e8af6c57
Tommy Morgan <jangel1@bu.edu>
2015-12-05 17:37:26 -0500

    CamTest2 all files

--- CamTest2/app/src/main/java/jayangel/camtest2/CamActivity.java
+++ CamTest2/app/src/main/java/jayangel/camtest2/CamActivity.java
@@ -7,21 +7,17 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.CalendarContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-
 import java.io.File;
+import java.util.Calendar;
 
 //Made using this tutorial: https://www.youtube.com/watch?v=k-3zXb7GteU
-
-
 
 public class CamActivity extends AppCompatActivity {
 
@@ -29,34 +25,52 @@ public class CamActivity extends AppCompatActivity {
     private static int TAKE_PICTURE = 1;
     private Uri imageUri;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cam);
-
-        Button cameraButton = (Button)findViewById(R.id.button_camera);
-        cameraButton.setOnClickListener(cameraListener);
-    };
-
+        //setContentView(R.layout.activity_cam);
+        takePhoto();
+        //Button cameraButton = (Button)findViewById(R.id.button_camera);
+        //cameraButton.setOnClickListener(cameraListener);
+    }; //end of onCreate
+/*
     private OnClickListener cameraListener = new OnClickListener(){
         public void onClick(View v){
-            takePhoto(v);
+            takePhoto();
         }
-    };
+    }; //end of onClickListener
+*/
+    /* ----------------------------------------------------------------*/
 
-    private void takePhoto(View v){
+
+    private void takePhoto(){
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         File photo;
-        photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"picture.jpg");
+        photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"picture_" + DateDemo.main()+ ".jpg");
         imageUri = Uri.fromFile(photo);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-        startActivityForResult(intent,TAKE_PICTURE);
-
+        startActivityForResult(intent, TAKE_PICTURE);
     }
+
+    /* ----------------------------------------------------------------*/
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode,Intent intent){
         super.onActivityResult(requestCode, resultCode,intent);
+
+
+        Calendar cal = Calendar.getInstance();
+        Intent intent5 = new Intent(Intent.ACTION_EDIT);
+        intent5.setData(CalendarContract.Events.CONTENT_URI);
+        intent5.putExtra("beginTime", cal.getTimeInMillis());
+        intent5.putExtra("allDay", true);
+        intent5.putExtra("rrule", "FREQ=YEARLY");
+        intent5.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
+        intent5.putExtra("title", "A Test Event from android app");
+        startActivity(intent5);
+
+       // takePhoto();
 
         if(resultCode == Activity.RESULT_OK) {
             Uri selectedImage = imageUri;
@@ -65,6 +79,8 @@ public class CamActivity extends AppCompatActivity {
             ImageView imageView = (ImageView)findViewById(R.id.image_camera);
             ContentResolver cr = getContentResolver();
             Bitmap bitmap;
+
+
 
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(cr,selectedImage);
---
+++ CamTest2/app/src/main/java/jayangel/camtest2/DateDemo.java
@@ -0,0 +1,19 @@
+package jayangel.camtest2;
+
+/**
+ * Created by jaybob320 on 12/5/15.
+ */
+import java.util.Date;
+
+public class DateDemo
+{
+    public static String main()
+    {
+        // Instantiate a Date object
+        Date date = new Date();
+
+        // display time and date using toString()
+        String magma = date.toString();
+        return magma;
+    }
+}
