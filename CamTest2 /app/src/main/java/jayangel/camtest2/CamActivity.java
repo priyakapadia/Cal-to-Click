99cfbc1aac5e8810f70a245908bfe3f06725b432
Tommy Morgan <jangel1@bu.edu>
2015-12-06 15:27:48 -0500

    opens camera only on startup, calendar works

--- CamTest2 /app/src/main/java/jayangel/camtest2/CamActivity.java
+++ CamTest2 /app/src/main/java/jayangel/camtest2/CamActivity.java
@@ -1,21 +1,20 @@
 package jayangel.camtest2;
 
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.CalendarContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.Calendar;
 
 //Made using this tutorial: https://www.youtube.com/watch?v=k-3zXb7GteU
 
@@ -24,16 +23,25 @@ public class CamActivity extends AppCompatActivity {
     private static String logtag = "CameraApp8";
     private static int TAKE_PICTURE = 1;
     private Uri imageUri;
-
+    public int initialCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam);
-        Button cameraButton = (Button)findViewById(R.id.button_camera);
-        cameraButton.setOnClickListener(cameraListener);
+        //opens the camera app on intial startup only
+        if(initialCounter < 1)
+        {
+            takePhoto();
+            initialCounter++;
+        }
+
+        //button for taking another picture
+            Button cameraButton = (Button)findViewById(R.id.button_camera);
+            cameraButton.setOnClickListener(cameraListener);
 
 
+        // button for exit
         Button btn1 = (Button) findViewById(R.id.button_exit);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,48 +80,39 @@ public class CamActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode,Intent intent){
-        super.onActivityResult(requestCode, resultCode,intent);
+        super.onActivityResult(requestCode, resultCode, intent);
 
-/*
-         //adds event to calendar
-        //Calendar cal = Calendar.getInstance();
-        Intent intent5 = new Intent(Intent.ACTION_EDIT);
+
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2016, 0, 19, 7, 30);
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2016, 0, 19, 8, 30);
+                Intent intent5 = new Intent(Intent.ACTION_INSERT);
         intent5.setData(CalendarContract.Events.CONTENT_URI);
-       // intent5.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime);
-        //intent5.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,endTime);
-        intent5.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
-        intent5.putExtra(CalendarContract.ExtendedProperties.NAME,"house");
-        intent5.putExtra(CalendarContract.ExtendedProperties.DESCRIPTION, "This is a sample description");
-        intent5.putExtra(CalendarContract.ExtendedProperties.EVENT_LOCATION, "My Guest House");
-        intent5.putExtra(CalendarContract.ExtendedProperties.RRULE, "FREQ=YEARLY");
-        //intent5.putExtra("beginTime", cal.getTimeInMillis());
-        //intent5.putExtra("allDay", true);
-        //intent5.putExtra("rrule", "FREQ=YEARLY");
-        //intent5.putExtra("endTime", cal.getTimeInMillis() + 60 * 60 * 1000);
-        //intent5.putExtra("title", "A Test Event from android app");
-        startActivity(intent5);
-        */
-/*
-        Intent intent5 = new Intent(Intent.ACTION_INSERT_OR_EDIT);
-        intent5.setType("vnd.android.cursor.item/event");
-        startActivity(intent);
-*/
+        intent5.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+        intent5.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+        intent5.putExtra(CalendarContract.Events.TITLE, "Hey Now");
+        //intent5.putExtra(CalendarContract.Events.
+                         startActivity(intent5);
+
+        //.putExtra("Title", "Yoga")
+                       // .putExtra(CalendarContract.DESCRIPTION, "Group class")
+                       // .putExtra(Events.EVENT_LOCATION, "The gym")
+                      //  .putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY)
+                      //  .putExtra(Intent.EXTRA_EMAIL, "rowan@example.com,trevor@example.com");
 
 
+
+
+// If image was taken and saved succesfully you go to main screen
         if(resultCode == Activity.RESULT_OK) {
+
             Uri selectedImage = imageUri;
-            getContentResolver().notifyChange(selectedImage,null);
-
-            ImageView imageView = (ImageView)findViewById(R.id.image_camera);
-            ContentResolver cr = getContentResolver();
-            Bitmap bitmap;
-
-
-
+            getContentResolver().notifyChange(selectedImage, null);
             try {
-                bitmap = MediaStore.Images.Media.getBitmap(cr,selectedImage);
-                imageView.setImageBitmap(bitmap);
+
                 Toast.makeText(CamActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+
             }catch(Exception e) {
                 Log.e(logtag, e.toString());
 
