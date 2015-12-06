807432bc465a905dd68797e306752c3e3384204f
Sneha Pradhan <snehapradhan@wireless1x-168-122-7-23.bu.edu>
2015-12-06 16:12:38 -0500

    Changed the icon and theme.

--- CamTest2 /app/build/generated/source/r/debug/jayangel/camtest2/R.java
+++ CamTest2 /app/build/generated/source/r/debug/jayangel/camtest2/R.java
@@ -1742,6 +1742,7 @@ containing a value of this type.
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
+        public static final int option_one=0x7f030001;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f060000;
